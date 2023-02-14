bc93e9c37809f412cae4b81e8cfa2485e43b924d
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-02-14 05:35:43 +0200

    relocated and refactored game class

---
+++ classes/game.rb
@@ -0,0 +1,16 @@
+require 'classes/item'
+
+class Game < Item
+    attr_accessor :multiplayer, :last_played
+    def initialize(multiplayer, last_played, publish_date, archive)
+        super(publish_date, archive)
+        @multiplayer = multiplayer
+        @last_played_at = last_played
+
+    end
+
+    def can_be_archived?
+       @last_played_at > 2 && super 
+    end
+
+end

54df2b4a26a2c4878508c247d1179b9af3218818
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-02-14 18:48:01 +0200

    Add testing folders and code

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../classes/music_album'
+require_relative '../classes/genre'
+
+describe MusicAlbum do
+  music_album = MusicAlbum.new(true, '2019-1-11')
+  genre = Genre.new('Gospel')
+  music_album.add_genre(genre)
+
+  it 'should be an instance of MusicAlbum' do
+    expect(music_album).to be_an_instance_of(MusicAlbum)
+  end
+
+  it 'archived should be false' do
+    expect(music_album.archive).to be false
+  end
+
+  it 'on_spotify should be true' do
+    expect(music_album.on_spotify).to be true
+  end
+
+  it 'genre property should be an instance of Genre' do
+    expect(music_album.genre).to be_an_instance_of(Genre)
+  end
+
+  it 'genre should be Gospel' do
+    expect(music_album.genre).to eq('Gospel')
+  end
+end

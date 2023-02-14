1aaa007ab0d34c6ff749b0a2381495f85db6d3d6
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-02-14 06:10:44 +0200

    initialize rspec and refactor game class

--- classes/game.rb
+++ classes/game.rb
@@ -3,8 +3,8 @@ require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played
 
-  def initialize(multiplayer, last_played, publish_date, archive)
-    super(publish_date, archive)
+  def initialize(multiplayer, last_played, publish_date = Time.now)
+    super(publish_date, false)
     @multiplayer = multiplayer
     @last_played_at = last_played
   end

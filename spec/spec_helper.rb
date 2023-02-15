62c4767cadab11cafe2c2612434eaf08f089bf87
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-02-15 14:50:03 +0200

    Add testing for music and update files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,5 @@ Bundler.setup(:default)
 require 'active_support/all'
 require_relative '../classes/author'
 require_relative '../classes/game'
+require_relative '../classes/genre'
+require_relative '../classes/music_album'

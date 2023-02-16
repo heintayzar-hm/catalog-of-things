a6a7bd3081d0500ef46acee2630be7d3b3c0edb3
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-02-16 11:05:20 +0200

    Genre testing changes made a required by reviewer

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -3,7 +3,6 @@ require_relative 'helper_spec'
 describe MusicAlbum do
   before :each do
     @music_album = MusicAlbum.new(true, '2017-01-01')
-    @music_album.genre = Genre.new(1, 'Gospel')
   end
   context 'given two arguments' do
     it 'is an object of MusicAlbum ' do
@@ -11,9 +10,6 @@ describe MusicAlbum do
     end
     it 'has the given data ' do
       expect(@music_album).to have_attributes(on_spotify: true, publish_date: Date.parse('2017-01-01'))
-    end
-    it 'can have one genre' do
-      expect(@music_album.genre).to be_an_instance_of(Genre)
     end
     it 'on_spotify should be true' do
       expect(@music_album.on_spotify).to be true
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-require 'active_support/all'
 require_relative '../classes/author'
 require_relative '../classes/game'
 require_relative '../classes/genre'

04bbcc55522b2f620fd24900f9439a4f31e7958d
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-02-16 16:07:35 +0200

    removed unfunctional options

--- app.rb
+++ app.rb
@@ -18,17 +18,14 @@ class App
     puts "Please Chose the following options:
                     1. List all books
                     2. List all music albums
-                    3. List all movies
-                    4. List of games
-                    5. List all genres
-                    6. List all labels
-                    7. List all authors
-                    8. List all sources
-                    9. Add a book
-                    10. Add a music album
-                    11. Add a movie
-                    12. Add a game
-                    13. Leave the catalog"
+                    3. List of games
+                    4. List all genres
+                    5. List all labels
+                    6. List all authors
+                    7. Add a book
+                    8. Add a music album
+                    9. Add a game
+                    10. Leave the catalog"
     puts ' '
   end
 
@@ -36,16 +33,13 @@ class App
     @options = [
       { user_choice: 1, action: :list_books },
       { user_choice: 2, action: :list_all_music_albums },
-      { user_choice: 3, action: :list_all_movies },
-      { user_choice: 4, action: :list_all_games },
-      { user_choice: 5, action: :list_all_genres },
-      { user_choice: 6, action: :list_all_labels },
-      { user_choice: 7, action: :list_all_authors },
-      { user_choice: 8, action: :list_all_sources },
-      { user_choice: 9, action: :add_a_book },
-      { user_choice: 10, action: :add_a_music_album },
-      { user_choice: 11, action: :add_a_movie },
-      { user_choice: 12, action: :add_a_game }
+      { user_choice: 3, action: :list_all_games },
+      { user_choice: 4, action: :list_all_genres },
+      { user_choice: 5, action: :list_all_labels },
+      { user_choice: 6, action: :list_all_authors },
+      { user_choice: 7, action: :add_a_book },
+      { user_choice: 8, action: :add_a_music_album },
+      { user_choice: 9, action: :add_a_game }
     ]
     action = @options.find { |option| option[:user_choice] == user_input }&.[](:action)
     send(action) if action
@@ -57,7 +51,7 @@ class App
     user_menu
     print('Please enter a number:')
     user_input = gets.chomp.to_i
-    return puts 'leave the catalog' if user_input >= 13
+    return puts 'leave the catalog' if user_input >= 10
 
     user_choice(user_input)
   end
